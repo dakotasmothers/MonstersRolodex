f8bfacf3a63b92c105d51a84ce75fc26fa06401b
Dakota Smothers <dsmothers192@gmail.com>
2020-08-21 05:53:13 -0500

    Updates

---
+++ precache-manifest.c3970daa6eb816ac0db3a185c0387b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c68d43868eadf68a69f66e1a02d1af9",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "f38a178ff6d37f121700",
+    "url": "/MonstersRolodex/static/css/main.59f2afdb.chunk.css"
+  },
+  {
+    "revision": "ae83830edc834d5ac8fa",
+    "url": "/MonstersRolodex/static/js/2.aeff5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.aeff5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38a178ff6d37f121700",
+    "url": "/MonstersRolodex/static/js/main.878c9d33.chunk.js"
+  },
+  {
+    "revision": "7fb1ec85ed84f38dd984",
+    "url": "/MonstersRolodex/static/js/runtime-main.6cfdf369.js"
+  }
+]);
